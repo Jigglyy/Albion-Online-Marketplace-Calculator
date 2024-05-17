5a562cdd5636aa5fcba67e24f6e22d39742224a4
Jiggly <Remikzfully@gmail.com>
2024-05-17 22:02:36 +0300

    tutorial page, and added a link to it in the index page

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index, table
+from .views import index, table, tutorial
 
 urlpatterns = [
     path("", index, name="index"),
     path("table/", table, name="table"),
+    path("tutorial/", tutorial, name="tutorial"),
 ]
--- main/views.py
+++ main/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'current_page': 'index'})
+
 
 def table(request):
-    return render(request, 'table.html')
+    return render(request, 'table.html', {'current_page': 'table'})
+
+def tutorial(request):
+    return render(request, 'tutorial.html', {'current_page': 'tutorial'})
