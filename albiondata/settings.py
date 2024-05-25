0f1bb5534e110cfe8f54d354e070ae03bd05b153
Jiggly <Remikzfully@gmail.com>
2024-05-25 23:43:48 +0300

    Revert "vercel fix attempt 2"
    This reverts commit 6f3853a260eb9a6852989fb2a9e2fb0fdc262ef3.

--- albiondata/settings.py
+++ albiondata/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,8 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),] #new  
+STATICFILES_DIRS = [BASE_DIR / "static/"] #new  
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
